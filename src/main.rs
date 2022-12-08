0e6681a835a189061c99277dfbc018e4a80a48f8
Jannik <theofficialjannik@gmail.com>
2022-12-08 20:48:38 +0100

    changed code

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,22 @@
 use merkletreelib::*;
 
 fn main() {
-    println!("Hello, world!");
-    
-    let tree = Tree::new(vec!["1".to_string(), "2".to_string(), "3".to_string(), "4".to_string(), "5".to_string(), "6".to_string(),
-                                          "7".to_string(), "8".to_string(), "9".to_string(), "10".to_string(), "11".to_string(), "12".to_string(),
-                                          "13".to_string(), "14".to_string(), "15".to_string(), "16".to_string(), "17".to_string(), "18".to_string(), 
-                                          "19".to_string(), "20".to_string(), "21".to_string(), "22".to_string(), "23".to_string(), "24".to_string()], 8);
 
+    // Generate a vector with 4104 values
+    let mut values = Vec::new();
+    let mut index = 0;
+    while index < 4104 {
+        let value = "V".to_string() + &index.to_string();
+        values.push(value);
+        index += 1;
+    }
+    
+    // Create a Tree with a height of 10 and a arity of 8
+    let tree = Tree::new(values, 8);
+
+    // Get and Print the height and the root of the Tree
     let height = tree.get_height();
-    let data = tree.get_data();
-    let leafs = tree.get_leafs();
     let root = tree.get_root();
-    let nodes = tree.get_nodes();
-
     println!("Root: {}, Height: {}", root, height);
-
-    for val in nodes {
-        println!("Node: {} \n", val);
-    }
-
-    for val in data {
-        println!("Input: {} \n", val);
-    }
-
-    for val in leafs {
-        println!("Leaf: {} \n", val);
-    }
-    
-    
-    
+        
 }
